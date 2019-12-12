fe480ee8eaa24c8dc9c032265cd499ca49bf8511
PythonStrangler <javajun90@mail.ru>
2019-12-12 18:37:28 +0300

    Решения задач с урока 2

---
+++ Testirovanie2-3-1.py
@@ -0,0 +1,41 @@
+from selenium import webdriver
+from selenium.webdriver.support.ui import Select
+import time
+import math
+
+def calc(x):
+    # Посчитать математическую функцию от x.
+    return str(math.log(abs(12*math.sin(int(x)))))
+
+try:
+    # Открыть страницу http://suninjuly.github.io/alert_accept.html
+    link = "http://suninjuly.github.io/alert_accept.html"
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    # Нажать на кнопку
+    button = browser.find_element_by_css_selector("button.btn").click()
+
+    # Принять confirm
+    confirm = browser.switch_to.alert
+    confirm.accept()
+
+    # На новой странице решить капчу для роботов, чтобы получить число с ответом
+    x_element = browser.find_element_by_id('input_value')
+    x = x_element.text
+    y = calc(x)
+
+    input1 = browser.find_element_by_id("answer")
+    input1.send_keys(y)
+
+    button = browser.find_element_by_css_selector("button.btn").click()
+
+    # Проверяем, что смогли зарегистрироваться
+    # ждем загрузки страницы
+    time.sleep(1)
+
+finally:
+    # ожидание чтобы визуально оценить результаты прохождения скрипта
+    time.sleep(10)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
---
+++ Testirovanie2-4-2.py
@@ -0,0 +1,42 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium import webdriver
+import time
+import math
+def calc(x):
+    # Посчитать математическую функцию от x.
+    return str(math.log(abs(12*math.sin(int(x)))))
+
+try:
+    link = "http://suninjuly.github.io/explicit_wait2.html"
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    # Дождаться, когда цена дома уменьшится до $100 (ожидание нужно установить не меньше 12 секунд)
+    text1 = WebDriverWait(browser, 12).until(
+        EC.text_to_be_present_in_element((By.ID, 'price'), '100')
+    )
+    # Нажать на кнопку "Book"
+    button = browser.find_element_by_id("book").click()
+
+    # Решить уже известную нам математическую задачу (используйте ранее написанный код) и отправить решение
+    x_element = browser.find_element_by_id('input_value')
+    x = x_element.text
+    y = calc(x)
+
+    input1 = browser.find_element_by_id("answer")
+    browser.execute_script("return arguments[0].scrollIntoView(true);", input1)
+    input1.send_keys(y)
+
+    button = browser.find_element_by_id("solve").click()
+
+    # Проверяем, что смогли зарегистрироваться
+    # ждем загрузки страницы
+    time.sleep(1)
+
+finally:
+    # ожидание чтобы визуально оценить результаты прохождения скрипта
+    time.sleep(10)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
